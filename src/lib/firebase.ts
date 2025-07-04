ba6aae71e99fa1a6d840ecb4e3fda84e818c093f
Gabriel Strand <gabbe200012@gmail.com>
2025-07-04 15:21:48 +0000

    I have put in the firebaseConfig values in the .env file. Now I want you

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -12,6 +12,6 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
-const db = getFirestore(app);
+const db = getFirestore(app, 'database1');
 
 export { db };
