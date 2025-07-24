423feef53b4faec5630d884a4c725d5f68f9f421
Gabriel Strand <gabbe200012@gmail.com>
2025-07-24 20:44:09 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <Card className="b

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,3 +1,4 @@
+
 "use client";
 
 import * as React from "react";
@@ -161,7 +162,7 @@ export default function DashboardPage() {
           <h1 className="text-3xl font-bold tracking-tight">Dashboard</h1>
         </div>
         <div className="grid gap-4 grid-cols-2 lg:grid-cols-4">
-          <Card className="bg-black text-white">
+          <Card>
             <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
               <CardTitle className="text-sm font-medium">
                 Month's Revenue
