7fdc6270ed3e7e3e99b14c529429a45528f60d32
Gabriel Strand <gabbe200012@gmail.com>
2025-07-25 21:39:41 +0000

    is there anything else needed before publishing and building the app?

--- next.config.ts
+++ next.config.ts
@@ -10,12 +10,6 @@ const withPWA = require('next-pwa')({
 
 const nextConfig: NextConfig = {
   /* config options here */
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
   images: {
     remotePatterns: [
       {
